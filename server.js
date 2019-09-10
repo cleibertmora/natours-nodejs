bc2822f803c04778a3887267493be83390d28315
soportebpa <soporte@smartmedic.tech>
2019-09-09 20:25:03 -0500

    Connect to atlas mongo db

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-//const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+//const DB = process.env.DATABASE_LOCAL;
 
 mongoose.connect( DB, {
     useNewUrlParser: true,
